74444adbb46d7b0b18bfdad1d9cd11060459bc0a
faizah1992 <faizah1992@gmail.com>
2020-04-10 11:13:04 -0500

    submitted

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,18 @@
+class AppearancesController < ApplicationController
+
+    def new 
+        @guests = Guest.all 
+        @episodes = Episode.all
+    end 
+
+    def create 
+        
+        @appearance = Appearance.create({
+            guest_id: params[:guest_id],
+            episode_id: params[:episode_id],
+            rating: params[:rating]
+        })
+     
+        redirect_to"/episodes/#{@appearance.episode_id}"
+    end
+end
